8e8b8d4c4f32e8ad43f712024f143c62b3d2c609
MoitaLuis <luismoita1337@gmail.com>
2021-01-21 18:11:28 -0300

    adicionada funcionalidade do botao

---
+++ api.js
@@ -0,0 +1,9 @@
+let item = document.querySelector('#task')
+let btn = document.querySelector('button')
+btn.addEventListener('click',addTodo)
+
+function addTodo(){
+  console.log(item.value)
+  console.log('texto')
+  }
+  console.log(btn)
